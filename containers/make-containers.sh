1b7a9d7512ba4b52699e9de42ce123eafd96dc8b
Lincoln Thurlow <lincoln@isi.edu>
2024-05-30 19:30:46 -0700

    added the branch
    Signed-off-by: Lincoln Thurlow <lincoln@isi.edu>

--- containers/make-containers.sh
+++ containers/make-containers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
-sudo docker build --build-arg CACHEBUST=`git rev-parse` -t docker.io/isilincoln/upf:latest -f Dockerfile.base .
+sudo docker build --build-arg CACHEBUST=`git rev-parse lt-dev` -t docker.io/isilincoln/upf:latest -f Dockerfile.base .
 sudo docker push docker.io/isilincoln/upf:latest 
